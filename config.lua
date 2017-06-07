fe827f0cef03b35a3b08eb266298e80619c432f1
Joachim Stolberg <joe.stolberg@gmx.de>
2017-06-07 22:33:19 +0200

    config file for maximum crane dimensions added

---
+++ config.lua
@@ -0,0 +1,8 @@
+-- Maximum crane height in blocks (12..n)
+towercrane.max_height = 24
+
+-- Maximum crane width in blocks (12..n)
+towercrane.max_width = 24
+
+-- Gain factor for the crane sound (0.0 to 1)
+towercrane.gain = 1
