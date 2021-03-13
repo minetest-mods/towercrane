599b2c93c639b3198a737f5af8e63c7b9ab8ce90
Joachim Stolberg <joe.stolberg@gmx.de>
2021-03-13 20:14:37 +0100

    Fix 'minetest.setting_* functions are deprecated' issues

--- config.lua
+++ config.lua
@@ -11,13 +11,13 @@
 
 
 -- Maximum crane height in blocks (8..n)
-towercrane.max_height = tonumber(minetest.setting_get("towercrane_max_height")) or 32
+towercrane.max_height = tonumber(minetest.settings:get("towercrane_max_height")) or 32
 
 -- Maximum crane width in blocks (8..n)
-towercrane.max_width = tonumber(minetest.setting_get("towercrane_max_width")) or 32
+towercrane.max_width = tonumber(minetest.settings:get("towercrane_max_width")) or 32
 
 -- Crane rope lenght in block (max_height .. max_height+x)
-towercrane.rope_length = tonumber(minetest.setting_get("towercrane_rope_length")) or 40
+towercrane.rope_length = tonumber(minetest.settings:get("towercrane_rope_length")) or 40
 
 -- Recipe available (true/false)
-towercrane.recipe = tonumber(minetest.setting_get("towercrane_recipe")) or true
+towercrane.recipe = tonumber(minetest.settings:get("towercrane_recipe")) or true
